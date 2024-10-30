f2becd82f4fe414953d1600e6f90a010d0c458f7
danieltsega <danieltsega6658@gmail.com>
2024-10-31 00:04:14 +0300

    feat: modify admin interface by adding fields and search

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Task
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at', 'updated_at')  # Fields to display in the list view
+    search_fields = ('title', 'description')  # Enable search by title and description
+    list_filter = ('category',)  # Filter by category
 
-# Register your models here.
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
