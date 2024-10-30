05c29f4d540c7a2f35a8e3e43c05e650fec72b55
danieltsega <danieltsega6658@gmail.com>
2024-10-31 01:31:20 +0300

    feat: implement rest viewset for the provider app

--- providers/views.py
+++ providers/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework import viewsets
+from .models import Provider
+from .serializers import ProviderSerializer
 
-# Create your views here.
+class ProviderViewSet(viewsets.ModelViewSet):
+    queryset = Provider.objects.all()
+    serializer_class = ProviderSerializer
+
+    @method_decorator(cache_page(60*15))  # Cache for 15 minutes
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
