7ec79ac32859f174158ad89b1dcbd6b3d8aedcec
danieltsega <danieltsega6658@gmail.com>
2024-10-30 23:41:09 +0300

    feat: add provider model in the app

--- providers/models.py
+++ providers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Provider(models.Model):
+    id = models.AutoField(primary_key=True)  # Unique identifier for each provider
+    name = models.TextField()  # Full name of the provider
+    contact_number = models.TextField()  # Phone number to reach the provider
+    email = models.TextField()  # Email address of the provider
+    tasks_offered = models.TextField()  # List or summary of tasks the provider can help with
+    location = models.TextField()  # City or area where the provider operates
+    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for when the provider was created
+
+    def __str__(self):
+        return self.name
+
