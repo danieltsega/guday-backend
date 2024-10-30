5f24aa51f7bb490a3cc0c77fb8f8fabef1ae6d08
danieltsega <danieltsega6658@gmail.com>
2024-10-31 00:00:06 +0300

    update: add cover image field

--- tasks/models.py
+++ tasks/models.py
@@ -11,6 +11,7 @@ class Task(models.Model):
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)  # Choices for task category
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  # Timestamp for when the task was last updated
+    cover_image = models.ImageField(upload_to='task_covers/', blank=True, null=True)  # Optional cover image
 
     def __str__(self):
         return self.title
