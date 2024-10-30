10546b8a44eb7ed755a39c7fc00e8ba5d9785b47
danieltsega <danieltsega6658@gmail.com>
2024-10-31 00:04:48 +0300

    feat: modify admin interface by adding fields and search

--- providers/admin.py
+++ providers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Provider
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_number', 'email', 'created_at')  # Fields to display in the list view
+    search_fields = ('name', 'contact_number', 'email')  # Enable search by name, contact number, and email
 
-admin.site.register(Provider)
+admin.site.register(Provider, ProviderAdmin)
