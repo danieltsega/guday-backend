58058cd8a93c25c38ee51bfeeb258c5e642bd8eb
danieltsega <danieltsega6658@gmail.com>
2024-10-31 00:29:07 +0300

    fix: fix import error in task

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Task
+
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'created_at', 'updated_at')  # Fields to display in the list view
     search_fields = ('title', 'description')  # Enable search by title and description
