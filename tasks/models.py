7184c20309b4730240e0424b00463897ae69539a
danieltsega <danieltsega6658@gmail.com>
2024-10-30 23:38:48 +0300

    feat: add task model in the app

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    CATEGORY_CHOICES = [
+        ('private', 'Private'),
+        ('government', 'Government'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()  # This will include steps, estimated time, and location
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)  # Choices for task category
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  # Timestamp for when the task was last updated
+
+    def __str__(self):
+        return self.title
+
