62e0db9fff87018e4a2a988a05a89acd2825a319
danieltsega <danieltsega6658@gmail.com>
2024-10-31 01:32:22 +0300

    feat: add rest viewset for tasks app

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    @method_decorator(cache_page(60*15))  # Cache for 15 minutes
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
